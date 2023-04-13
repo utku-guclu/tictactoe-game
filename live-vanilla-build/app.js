7949ab2f4c35dd1f81d5a9335a4911f06a533de1
utku-guclu <stanger307@live.com>
2023-04-13 08:30:40 -0700

    messy code but working :=

--- live-vanilla-build/app.js
+++ live-vanilla-build/app.js
@@ -6,6 +6,10 @@ const App = {
     resetBtn: document.querySelector('[data-id="reset-btn"]'),
     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
     squares: document.querySelectorAll('[data-id="square"]'),
+    modal: document.querySelector('[data-id="modal"]'),
+    modalText: document.querySelector('[data-id="modal-text"]'),
+    modalBtn: document.querySelector('[data-id="modal-btn"]'),
+    turn: document.querySelector('[data-id="turn"]'),
   },
 
   state: {
@@ -16,12 +20,15 @@ const App = {
     const playerMoves = moves
       .filter((move) => move.isPlayer)
       .map((move) => move.squareId);
-    const cpuMoves = moves.filter((move) => !move.isPlayer);
+    const cpuMoves = moves
+      .filter((move) => !move.isPlayer)
+      .map((move) => move.squareId);
 
     // winning pattern
     const winningPatterns = [
       [1, 2, 3],
       [1, 5, 9],
+      [3, 5, 7],
       [1, 4, 7],
       [2, 5, 8],
       [3, 6, 9],
@@ -33,14 +40,16 @@ const App = {
 
     winningPatterns.forEach((pattern) => {
       const playerWins = pattern.every((v) => playerMoves.includes(v));
-      if (playerWins) {
-        console.log("player wins");
-      }
+      const cpuWins = pattern.every((v) => cpuMoves.includes(v));
+
+      if (playerWins) winner = 1;
+      if (cpuWins) winner = 2;
     });
 
     return {
-      status: "in-progress",
-      winner,
+      status:
+        moves.length === 9 || winner !== null ? "complete" : "in-progress", // in-progress | complete
+      winner, // true | false | null
     };
   },
 
@@ -49,7 +58,17 @@ const App = {
   },
 
   registerEventListeners() {
-    const { menu, menuItems, resetBtn, newRoundBtn, squares } = App.$;
+    const {
+      menu,
+      menuItems,
+      resetBtn,
+      newRoundBtn,
+      squares,
+      modal,
+      modalBtn,
+      modalText,
+      turn,
+    } = App.$;
 
     menu.addEventListener("click", () => {
       menuItems.classList.toggle("hidden");
@@ -60,6 +79,12 @@ const App = {
     newRoundBtn.addEventListener("click", () => {
       console.log("1");
     });
+    modalBtn.addEventListener("click", () => {
+      App.state.moves = [];
+      squares.forEach((square) => square.replaceChildren());
+      modal.classList.add("hidden");
+    });
+
     squares.forEach((square) =>
       square.addEventListener("click", () => {
         this.getGameStatus(App.state.moves);
@@ -80,9 +105,21 @@ const App = {
         const currentPlayer =
           this.state.moves.length === 0 ? true : isPlayer(lastMove.isPlayer);
 
-        const icon = document.createElement("i");
+        const turnLabel = document.createElement("p");
+        const turnIcon = document.createElement("i");
+        const squareIcon = document.createElement("p");
         const iconXO = currentPlayer ? "fa-x" : "fa-o";
-        icon.classList.add("fa-solid", iconXO, "yellow");
+        const turnIconXO = !currentPlayer ? "fa-x" : "fa-o";
+        const turnIconColor = currentPlayer ? "turquoise" : "yellow";
+        const iconColor = currentPlayer ? "yellow" : "turquoise";
+        const nextTurn = !currentPlayer ? "player" : "computer";
+
+        turnLabel.innerText = `${nextTurn} is up!`;
+
+        squareIcon.classList.add("fa-solid", iconXO, iconColor);
+        turnIcon.classList.add("fa-solid", turnIconXO, turnIconColor);
+        turnLabel.classList = turnIconColor;
+        turn.replaceChildren(turnIcon, turnLabel);
 
         this.state.moves.push({
           squareId: +square.id,
@@ -91,7 +128,22 @@ const App = {
 
         this.state.currentPlayer = isPlayer(currentPlayer);
 
-        square.replaceChildren(icon);
+        square.replaceChildren(squareIcon);
+        // check if there is a winner or tie game
+        const game = App.getGameStatus(App.state.moves);
+
+        if (game.status === "complete") {
+          let message;
+          if (game.winner === 1) {
+            message = `Player wins!`;
+          } else if (game.winner === 2) {
+            message = `Computer wins!`;
+          } else {
+            message = `TIE!`;
+          }
+          modalText.textContent = message;
+          modal.classList.remove("hidden");
+        }
       })
     );
   },
